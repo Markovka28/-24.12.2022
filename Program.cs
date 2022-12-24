3fa0913c0659e78096dc42f8694d4dec4f636007
Valery_Krylov <supermaaaan@mail.ru>
2022-12-24 16:38:08 +0500

    Решение задачи 21

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿/*Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, 
-является ли оно палиндромом.*/
+является ли оно палиндромом.
     
 Console.Write("Введите пятизначное число: ");
             int n = Convert.ToInt32(Console.ReadLine());
@@ -18,4 +18,27 @@ Console.Write("Введите пятизначное число: ");
             else
             {
                 Console.WriteLine($"Число не явлется палиндромом");
-            }
+            }*/
+
+            
+
+/*Задача 21. Напишите программу, которая принимает на вход координаты двух точек 
+и находит расстояние между ними в 3D пространстве.
+
+Console.Clear(); 
+Console.Write("Введите X1: ");
+int x1=int.Parse(Console.ReadLine());
+Console.Write("Введите Y1: ");
+int y1=int.Parse(Console.ReadLine());
+Console.Write("Введите X2: ");
+int x2=int.Parse(Console.ReadLine());
+Console.Write("Введите Y2: ");
+int y2=int.Parse(Console.ReadLine());
+Console.Write("Введите W1: ");
+int w1=int.Parse(Console.ReadLine());
+Console.Write("Введите W2: ");
+int w2=int.Parse(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2) + Math.Pow(w1-w2, 2));
+
+Console.WriteLine($"d={d:f5}");*/
